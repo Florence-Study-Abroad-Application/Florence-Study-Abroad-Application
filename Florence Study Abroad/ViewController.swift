4e4e23306246b10d7da2dcfb64a5ce5766dc17dc
Brett Thomas <bthomas8@live.maryville.edu>
2018-05-01 10:59:13 -0500

    Functionality complete

--- Florence Study Abroad/ViewController.swift
+++ Florence Study Abroad/ViewController.swift
@@ -14,18 +14,87 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //Outlets
+    @IBOutlet weak var mainMenuButton: UIButton!
+    
+    
+    
+    //Home screen functions
+    @IBAction func pressedMainMenu(_ sender: Any) {
+        performSegue(withIdentifier: "toMainMenu", sender: mainMenuButton)
+    }
     
     
     
     
+    //Main Menu functions
+    @IBAction func pressedHome(_ sender: Any) {
+        performSegue(withIdentifier: "toHome", sender: nil)
+    }
+    
+    @IBAction func pressedRegistration(_ sender: Any) {
+        performSegue(withIdentifier: "toRegistration", sender: nil)
+    }
+    
+    @IBAction func pressedPreDeparture(_ sender: Any) {
+        performSegue(withIdentifier: "toPreDeparture", sender: nil)
+    }
+    
+    @IBAction func pressedAbroad(_ sender: Any) {
+        performSegue(withIdentifier: "toAbroad", sender: nil)
+    }
+    
+    @IBAction func pressedSettings(_ sender: Any) {
+        performSegue(withIdentifier: "toSettings", sender: nil)
+    }
     
     
     
     
+    //Pre Departure functions
+    @IBAction func pressedCulture(_ sender: Any) {
+        performSegue(withIdentifier: "toCulture", sender: nil)
+    }
+    
+    @IBAction func pressedMaps(_ sender: Any) {
+        performSegue(withIdentifier: "toMaps", sender: nil)
+    }
+    
+    @IBAction func pressedContactInfo(_ sender: Any) {
+        performSegue(withIdentifier: "toContactInformation", sender: nil)
+    }
+    
+    @IBAction func pressedLanguage(_ sender: Any) {
+        performSegue(withIdentifier: "toLanguage", sender: nil)
+    }
+    
+    @IBAction func pressedFinancing(_ sender: Any) {
+        performSegue(withIdentifier: "toFinancing", sender: nil)
+    }
+    
+    @IBAction func pressedFAQs(_ sender: Any) {
+        performSegue(withIdentifier: "toFAQ's", sender: nil)
+    }
     
     
     
+    //Abroad functions
+    @IBAction func pressedAbroadCulture(_ sender: Any) {
+        performSegue(withIdentifier: "toAbroadCulture", sender: nil)
+    }
+    
+    @IBAction func pressedAbroadMaps(_ sender: Any) {
+        performSegue(withIdentifier: "toAbroadMaps", sender: nil)
+    }
+    
+    @IBAction func pressedAbroadEmergency(_ sender: Any) {
+        performSegue(withIdentifier: "toAbroadEmergency", sender: nil)
+    }
+    
+    @IBAction func pressedAbroadLanguage(_ sender: Any) {
+        performSegue(withIdentifier: "toAbroadLanguage", sender: nil)
+    }
     
     
     override func didReceiveMemoryWarning() {
